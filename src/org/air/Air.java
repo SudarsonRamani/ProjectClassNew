d7a40f4f31dcd860fe8281f59a4509d72f498700
SudarsonRamani <sudarson.mba@gmail.com>
2021-09-08 12:19:25 +0530

    12.20PM

--- src/org/air/Air.java
+++ src/org/air/Air.java
@@ -4,8 +4,10 @@ public class Air {
 	
 	public void aeroPlane() {
 		System.out.println("aeroPlane");
+		System.out.println("aeroPlane");
 	}
 	public void heliCopter() {
+		System.out.println("heliCopter");
 		System.out.println("heliCopter");
 	}
 
